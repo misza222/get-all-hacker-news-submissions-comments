a346acd06bf4d858eefeef9dedd90c398b28a33e
Michal Pawlowski <misza222@gmail.com>
2016-01-12 16:32:32 +0100

    Fixed bug when db empty

--- hacker_news.py
+++ hacker_news.py
@@ -18,4 +18,7 @@ def init_db(dbfile, schema):
 def get_last_ts(db):
     cursor = db.cursor().execute('SELECT MAX(created_at) FROM hn_submissions;')
     result = cursor.fetchone()
-    return calendar.timegm(time.strptime(result[0] + ' UTC', '%Y-%m-%d %H:%M:%S %Z'))
+    try:
+        return calendar.timegm(time.strptime(result[0] + ' UTC', '%Y-%m-%d %H:%M:%S %Z'))
+    except Exception:
+        return calendar.timegm((1990,1,1,0,0,0,))
